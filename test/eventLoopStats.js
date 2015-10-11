f7c7836f930a595c48706f06345c656c5c6188b1
Ben Ripkens <bripkens.dev@gmail.com>
2015-10-11 08:44:32 +0200

    Actually verify that the uv check handle was executed

--- test/eventLoopStats.js
+++ test/eventLoopStats.js
@@ -7,12 +7,20 @@ describe('eventLoopStats', function() {
     expect(eventLoopStats.sense).to.be.a('function');
   });
 
-  it('should return stats via the sense function', function() {
-    var stats = eventLoopStats.sense();
-    expect(stats.num).to.be.a('number');
-    expect(stats.sum).to.be.a('number');
-    expect(stats.max).to.be.a('number');
-    expect(stats.min).to.be.a('number');
+  it('should return stats via the sense function', function(done) {
+    setTimeout(function() {
+      // provoke execution on the default loop
+    }, 500);
+
+    setTimeout(function() {
+      var stats = eventLoopStats.sense();
+      expect(stats.num).to.be.a('number');
+      expect(stats.num).to.be.gt(0);
+      expect(stats.sum).to.be.a('number');
+      expect(stats.max).to.be.a('number');
+      expect(stats.min).to.be.a('number');
+      done();
+    }, 1000);
   });
 
 });
